ec8dd58cc5f413260ac23b39c499b00cee4c45f7
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-20 15:42:49 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express      = require('express');
 const logger       = require('morgan');
 const bodyParser   = require('body-parser');
-var homeController = require('./controllers/homeController')
+// var homeController = require('./controllers/homeController')
 
 //Set up express app
 const app = express();
@@ -20,12 +20,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 // Default catch-all  route with welcome message
-// app.get('*', (req, res) => res.status(200).send({
-//   message: 'Welcome to the beginning of Nerd Bay.'
-// }));
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of Nerd Bay.'
+}));
 
 //Fire Controllers
-homeController(app);
+// homeController(app);
 
 app.listen(3000);
 console.log('Listening to port 3000...')
