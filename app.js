d3c3939a6f47758dc0460bbd10121878475d930e
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-20 10:49:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const logger  = require('morgan');
+const express    = require('express');
+const logger     = require('morgan');
 const bodyParser = require('body-parser');
 
 //Set up express app
@@ -8,14 +8,22 @@ const app = express();
 // Log requests to the console.
 app.use(logger('dev'));
 
+//Set up template engine
+app.set('view engine', 'ejs');
 
-// Pare incoming requests data
+// Static Files
+app.use(express.static('./public'));
+
+// Parse incoming requests data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded([ extended: false]));
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Default catch-all  route with welcome message
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of Nerd Bay.'
 }));
 
+
+app.listen(3000);
+console.log('Listening to port 3000...')
 module.exports = app;
